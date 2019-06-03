40ee53bcf8461dd916ca39426f518fe6629cd9d2
joris kleijn <34738260+jorisklijen@users.noreply.github.com>
2019-06-04 00:35:05 +0200

    00:34 04-06-2019
    _animaites fix en pucnch

---
+++ OneHitWonder_2D/Assets/Scripts/Player_script/PlayerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [Header("inputs")]
+    public KeyCode havy;
+    public KeyCode licht;
+    public KeyCode richt;
+    public KeyCode left;
+
+    float dirX; 
+    public float moveSpeed;
+
+    Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
+
+        if (dirX != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Smal_punch")) //in de "" de naam van de anim
+        {
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        
+        //Dit stuk werkt 100%
+        if (Input.GetKeyDown(licht) && !anim.GetCurrentAnimatorStateInfo(0).IsName("Smal_punch")) //in de "" de naam van de anim
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetTrigger("smalPunch"); // de naam van de trigger 
+        }
+    }
+    
+}
