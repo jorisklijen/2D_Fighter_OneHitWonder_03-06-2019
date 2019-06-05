35f1bd246fc943d911f8895cceaacd114fadf015
jorisklijen <34738260+jorisklijen@users.noreply.github.com>
2019-06-05 21:32:17 +0200

    als er ieets kapot is

---
+++ OneHitWonder_2D/Assets/Scripts/Player_script/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerControler2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Move our characte
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false );
+        jump = false;
+    }
+}
